43c005f1a7426749435192cef05bfab7544acd88
hutsulol <130280679+hutsulol@users.noreply.github.com>
2026-04-23 09:42:45 +0300

    Raise DNA Study side panels by ~100px on Y axis

--- src/StarterPlayerScripts/Mercenaries/DNAStudyPage.client.lua
+++ src/StarterPlayerScripts/Mercenaries/DNAStudyPage.client.lua
@@ -711,8 +711,8 @@ local function openDNAStudyPage(ctx)
 	local backBtnRef, leftColumnRef, rightColumnRef
 	local BACK_BTN_Y = 39
 	local SIDE_MENU_SCALE = 2
-	local LEFT_SIDE_MENU_RAISE_Y = 16
-	local RIGHT_SIDE_MENU_RAISE_Y = 30
+	local LEFT_SIDE_MENU_RAISE_Y = 116
+	local RIGHT_SIDE_MENU_RAISE_Y = 130
 
 	local function updateResponsiveScale()
 		local size = screenGui.AbsoluteSize
